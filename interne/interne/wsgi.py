b1d3d6311215a51388ccd59e06281b74aff69545
yaw-intern <abuaku16@gmail.com>
2023-05-02 00:50:59 +0100

    github deployment settings

--- interne/interne/wsgi.py
+++ interne/interne/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+settings_module = 'azure_project.production' if 'PRODUCTION' in os.environ else 'azure_project.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interne.settings')
 
 application = get_wsgi_application()
