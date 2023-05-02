25b975c0c9f1ff883b4eab15d1d26983a40eddfb
yaw-intern <abuaku16@gmail.com>
2023-05-02 16:59:03 +0100

    added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
