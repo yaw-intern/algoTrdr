0208268c38325dfeeca425ca4d98530fb1caf645
yaw-intern <abuaku16@gmail.com>
2023-05-03 11:20:07 +0100

    hmm

--- interne/settings.py
+++ interne/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'algotrader',
         'USER': 'abuakunathan',
-        'PASSWORD': os.environ.get('PASSWORD'),
+        'PASSWORD': os.environ['DB_PASS'],
         'HOST': 'algotraddb.mysql.database.azure.com',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
